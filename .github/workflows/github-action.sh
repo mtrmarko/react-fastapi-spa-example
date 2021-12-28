23d6d2c248404dde85f46139574bcc0f28a8cecc
Marko Mitrovic <marko@noemail.com>
2021-12-28 00:43:17 -0800

    fixes to github action script

--- .github/workflows/github-action.sh
+++ .github/workflows/github-action.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-cd ~/repos
-if [ -d ~/repos/react-fastapi-spa-example ]; then
-	cd ~/repos/react-fastapi-spa-example
-	git clean -Xdf
-	git clean -df
-	git pull
-	setup_backend_service
-	setup_frontend_service
-else
-	git clone https://github.com/mtrmarko/react-fastapi-spa-example.git
-	cd ~/repos/react-fastapi-spa-example
-	setup_backend_service
-	setup_frontend_service	
-fi
-
-##TODO: check for service existence
-
 function setup_backend_service()
 {
 	cd backend
@@ -39,3 +22,21 @@ function setup_frontend_service()
 	./managesvc.sh install marko
 	cd ~/repos/react-fastapi-spa-example
 }
+
+cd ~/repos
+if [ -d ~/repos/react-fastapi-spa-example ]; then
+	cd ~/repos/react-fastapi-spa-example
+	git clean -Xdf
+	git clean -df
+	git pull
+	setup_backend_service
+	setup_frontend_service
+else
+	git clone https://github.com/mtrmarko/react-fastapi-spa-example.git
+	cd ~/repos/react-fastapi-spa-example
+	setup_backend_service
+	setup_frontend_service	
+fi
+
+##TODO: check for service existence
+
